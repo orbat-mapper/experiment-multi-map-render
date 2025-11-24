7bab65209d1b2bbe361fb1398a0a4148e3b262d6
KMF <kjellmf@gmail.com>
2025-11-24 20:43:57 +0100

    Add shadcn-vue

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,35 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import {
+  ResizableHandle,
+  ResizablePanel,
+  ResizablePanelGroup,
+} from "@/components/ui/resizable";
+</script>
 
 <template>
-  <div>Hello world</div>
+  <div class="h-full w-full">
+    <ResizablePanelGroup direction="horizontal" class="h-full w-full">
+      <ResizablePanel :default-size="20" class="">
+        <div class="flex h-[200px] items-center justify-center p-6">
+          <span class="font-semibold">One</span>
+        </div>
+      </ResizablePanel>
+      <ResizableHandle />
+      <ResizablePanel :default-size="50">
+        <ResizablePanelGroup direction="vertical">
+          <ResizablePanel :default-size="25">
+            <div class="flex h-full items-center justify-center p-6">
+              <span class="font-semibold">Two</span>
+            </div>
+          </ResizablePanel>
+          <ResizableHandle />
+          <ResizablePanel :default-size="75">
+            <div class="flex h-full items-center justify-center p-6">
+              <span class="font-semibold">Three</span>
+            </div>
+          </ResizablePanel>
+        </ResizablePanelGroup>
+      </ResizablePanel>
+    </ResizablePanelGroup>
+  </div>
 </template>
